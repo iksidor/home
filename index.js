6d07683a929a5c19097b0ed05cb5f4bc3b7be025
iksidor <patrick.pappers@googlemail.com>
2018-05-20 16:27:07 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 jQuery(document).ready(
     function(){
-        console.log('blabla');
+        //console.log('blabla');
+
+        var user = jsHue().bridge('192.168.178.36').user('qt37YDwwAFuP7WjtpA1dFmCA6PejQSNYfloaoyTA');
+
+        jQuery('.iris').on('click', ()=>{
+            user.getLight(6).then(lightConfig => {
+               user.setLightState(6, {on: !lightConfig.state.on});
+            });
+        });
     }
+
+    
 )
